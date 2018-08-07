d00c9c42f3ea83a1d22cbf30e179206019c9cdea
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2018-08-07 21:58:56 +0300

    Add Chessie
    Add the Chessie error-handling library. It doesn't include all the
    necessary functions and operators. Add some additional functions and
    operators. Also, add simple support for the State monad.

---
+++ FsBowling/Trial.fs
@@ -0,0 +1,11 @@
+﻿module Chessie.ErrorHandling.Trial
+
+let rec traverse f list =
+
+    let cons head tail = head :: tail
+
+    match list with
+    | [] -> [] |> ok
+    | head :: tail -> cons <!> (f head) <*> (traverse f tail)
+
+let sequence list = list |> traverse id
