1dd9befc83355de36b9f107d8fa1ad84a8f0d9b9
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2018-11-25 14:14:57 +0200

    Add a test for rolling on a frame level

--- FsBowling.Tests/DSL.fs
+++ FsBowling.Tests/DSL.fs
@@ -1,15 +1,67 @@
 ﻿[<AutoOpen>]
 module FsBowling.DSL
 
+open FSharpPlus
 open FSharpPlus.Data
+
+let with' config reader =
+    Reader.run reader config
 
 let createConfig (numPins, numFrames, nameLength, playerCount) =
     Config.create numPins numFrames nameLength playerCount
 
 let createPlayerName = PlayerName.create
 
-let with' config reader =
-    Reader.run reader config
+let normalizeState state =
+    Reader.ask |>> Config.numberOfPins |>> (fun numPins ->
+        let halfPins = numPins / 2
+        let maxPins = numPins + 1
+
+        match state with
+        | InProgress score -> InProgress (abs score % numPins)
+        | Open (score1, score2) -> Open (abs score1 % halfPins, abs score2 % halfPins)
+        | LastStrikeInProgress2 score -> LastStrikeInProgress2 (abs score % maxPins)
+        | LastStrike (score1, score2) -> Open (abs score1 % maxPins, abs score2 % maxPins)
+        | Spare score -> Spare (abs score % numPins)
+        | LastSpareInProgress score -> LastSpareInProgress (abs score % numPins)
+        | LastSpare (score1, score2) -> LastSpare (abs score1 % numPins, abs score2 % maxPins)
+        | _ -> state)
+
+let createFrame (num, state) = monad {
+    let! config = Reader.ask
+    let numPins = config |> Config.numberOfPins
+    let numFrames = config |> Config.numberOfFrames
+    let dummy = InvalidScore 0
+
+    return monad {
+        let! frame = Frame.create num |> with' config
+        let roll score frame = Frame.roll score frame |> with' config
+
+        return!
+            match state |> normalizeState |> with' config with
+            | NotStarted ->
+                frame |> Ok
+            | InProgress score ->
+                frame |> roll score
+            | Open (score1, score2) ->
+                frame |> roll score1 >>= roll score2
+            | Strike ->
+                frame |> roll numPins
+            | LastStrikeInProgress1 when num = numFrames ->
+                frame |> roll numPins
+            | LastStrikeInProgress2 score when num = numFrames ->
+                frame |> roll numPins >>= roll score
+            | LastStrike (score1, score2) when num = numFrames ->
+                frame |> roll numPins >>= roll score1 >>= roll score2
+            | Spare score ->
+                frame |> roll score >>= roll (numPins - score)
+            | LastSpareInProgress score when num = numFrames ->
+                frame |> roll score >>= roll (numPins - score)
+            | LastSpare (score1, score2) when num = numFrames ->
+                frame |> roll score1 >>= roll (numPins - score1) >>= roll score2
+            | _ -> dummy |> Error
+    }
+}
 
 let (>==>) result test =
     match result with
--- FsBowling.Tests/FrameTests.fs
+++ FsBowling.Tests/FrameTests.fs
@@ -15,4 +15,57 @@ let tests =
                     (frame |> Frame.state) = FrameState.NotStarted
                 | Error error ->
                     error = InvalidFrameNumber num
+
+        testProperty "A roll advances a frame to a new state if and only if the score is valid" <| fun configParams num state score ->
+            (createConfig configParams) >>==> fun config ->
+                (createFrame (num, state) |> with' config) >==> fun frame ->
+                    let state = state |> normalizeState |> with' config
+                    let numPins = config |> Config.numberOfPins
+                    let numFrames = config |> Config.numberOfFrames
+
+                    match frame |> Frame.roll score |> with' config with
+                    | Ok frame ->
+                        match state with
+                        | NotStarted ->
+                            frame |> Frame.number = num &&
+                            frame |> Frame.state =
+                                if score = numPins
+                                then if num = numFrames then LastStrikeInProgress1 else Strike
+                                else InProgress score
+                        | InProgress score' ->
+                            frame |> Frame.number = num &&
+                            frame |> Frame.state =
+                                if score + score' = numPins
+                                then if num = numFrames then LastSpareInProgress score' else Spare score'
+                                else Open (score', score)
+                        | Open _ ->
+                            frame |> Frame.number = num + 1 &&
+                            frame |> Frame.state = InProgress score
+                        | Strike | Spare _ ->
+                            frame |> Frame.number = num + 1 &&
+                            num < numFrames &&
+                            frame |> Frame.state = InProgress score
+                        | LastStrikeInProgress1 ->
+                            frame |> Frame.number = num &&
+                            num = numFrames &&
+                            frame |> Frame.state = LastStrikeInProgress2 score
+                        | LastStrikeInProgress2 score' ->
+                            frame |> Frame.number = num &&
+                            num = numFrames &&
+                            frame |> Frame.state = LastStrike (score', score)
+                        | LastSpareInProgress score' ->
+                            frame |> Frame.number = num &&
+                            num = numFrames &&
+                            frame |> Frame.state = LastSpare (score', score)
+                        | _ -> false
+                    | Error error ->
+                        match state with
+                        | LastStrike _ | LastSpare _ ->
+                            error = RollAfterLastFrame
+                        | Open _ when num = numFrames ->
+                            error = RollAfterLastFrame
+                        | NotStarted | Open _ | Strike | Spare _ | LastStrikeInProgress1 | LastStrikeInProgress2 _ | LastSpareInProgress _ ->
+                            error = InvalidScore score
+                        | InProgress score' ->
+                            error = InvalidScore (score' + score)
     ]
